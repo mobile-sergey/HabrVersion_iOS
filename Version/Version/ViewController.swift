17490caa98d9b9892d6519759a324b0f3789424e
Sergey Lavrov <lavrov-sergey@yandex.ru>
2023-08-07 18:15:46 +0300

    Add app logic and UI for Main.storyboard

--- Version/Version/ViewController.swift
+++ Version/Version/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func roll(_ sender: Any) {
+        label.text = String(Int.random(in: 1..<7))
+    }
+    
 }
-
